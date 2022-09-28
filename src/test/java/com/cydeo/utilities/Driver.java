0ed81192de2c61a880eaa5c3b9182a1543664901
fatihcam46 <fatih.cam.bahar@gmail.com>
2022-09-28 23:38:37 +0100

    cydeo-cucumber first

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -15,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Driver {
     /*   Creating a private constructor, so we are closing
-      access to the object of this class from outside the class
+      access to the object of this class from outside the class.
        */
     private Driver() {
     }
